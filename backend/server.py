6adbec0421c12a8e2fcc4b6ce62149e398a55101
Shehzad Khan <khanshehzad2698@gmail.com>
2026-08-17 19:56:31 +0530

    Fix CORS for Vercel frontend

--- backend/server.py
+++ backend/server.py
@@ -205,8 +205,10 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=[
+        "https://nexbuild-bice.vercel.app",
+    ],
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
